33996aa84e2b66e88f2cea975662b74784974f36
Lelor <lrodrigues0804@gmail.com>
2022-10-18 19:57:29 -0400

    [WIP] adding rudimentary camera controls to the coordinate system

--- OpenGL/res/shaders/Basic.shader
+++ OpenGL/res/shaders/Basic.shader
@@ -6,10 +6,12 @@ layout(location = 1) in vec2 texCoord;
 
 out vec2 v_TexCoord;
 
-uniform mat4 u_transform;
+uniform mat4 u_Model;
+uniform mat4 u_View;
+uniform mat4 u_Projection;
 
 void main() {
-    gl_Position = u_transform * position;
+    gl_Position = u_Projection * u_View * u_Model * position;
     v_TexCoord = texCoord;
 };
 
@@ -24,5 +26,15 @@ uniform sampler2D u_Texture;
 
 void main() {
 	vec4 texColor = texture(u_Texture, v_TexCoord);
-	color = texColor;
+	if (texColor.w > 0)
+	{
+		color = texColor;
+	}
+	else
+	{
+		if (v_TexCoord.x < 0.05 || v_TexCoord.y < 0.05 || v_TexCoord.x > 0.95 || v_TexCoord.y > 0.95)
+			color = vec4(0.0, 0.0, 1.0, 1.0);
+		else
+			color = vec4(1.0, 0.0, 0.0, 1.0);
+	}
 };
--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -18,6 +18,9 @@
 
 #define PI 3.141592653f
 
+
+
+
 int main()
 {
     GLFWwindow* window;
@@ -47,48 +50,139 @@ int main()
         return -1;
     }
 
-    const float data[] = {
-        -.5f, -.5f,  .0f,  .0f,
-         .5f, -.5f, 1.0f,  .0f,
-         .5f,  .5f, 1.0f, 1.0f,
-        -.5f,  .5f,  .0f, 1.0f
+    float cube[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, // 0 0 0
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, // 1 0 0
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // 1 1 0
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // 0 1 0
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // 0 0 1
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // 1 0 1
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, // 1 1 1
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, // 0 1 1
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // 0 1 1
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // 0 1 0
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // 0 0 0
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // 0 0 1
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // 1 1 1
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // 1 1 0
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // 1 0 0
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // 1 0 1
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, // 0 0 0
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f, // 1 0 0
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, // 1 0 1
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, // 0 0 1
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // 0 1 0
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, // 1 1 0
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, // 1 1 1
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, // 0 1 1
     };
+
+    glm::vec3 cubePositions[] = {
+    glm::vec3(0.0f,  0.0f,  0.0f),
+    glm::vec3(2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3(2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3(1.3f, -2.0f, -2.5f),
+    glm::vec3(1.5f,  2.0f, -2.5f),
+    glm::vec3(1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
+
 
     unsigned int indexBuffer[] = {
         0, 1, 2,
-        2, 3, 0
+        2, 3, 0,
+
+        4, 5, 6,
+        6, 7, 4,
+
+        8, 9, 10,
+        10, 11, 8,
+
+        12, 13, 14,
+        14, 15, 12,
+
+        16, 17, 18,
+        18, 19, 16,
+
+        20, 21, 22,
+        22, 23, 20
     };
     GLCall(glEnable(GL_BLEND));
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
     VertexArray va;
-    VertexBuffer vb(data, 4 * 4 * sizeof(float));
+
+    VertexBuffer vb(cube, 5 * 24 * sizeof(float));
     VertexBufferLayout layout;
-    layout.Push<float>(2);
+    layout.Push<float>(3);
     layout.Push<float>(2);
     va.AddBuffer(vb, layout);
-    IndexBuffer ib(indexBuffer, 6);
+    IndexBuffer ib(indexBuffer, 36);
 
     Shader shader("res/shaders/Basic.shader");
     Texture texture("res/textures/sample.png");
     texture.Bind(0);
-    shader.Bind();
+    //shader.Bind();
 
-    va.Unbind();
-    shader.Unbind();
-    vb.Unbind();
-    ib.Unbind();
-
+    glm::mat4 view = glm::mat4(1.0f);
+    // note that we're translating the scene in the reverse direction of where we want to move
+    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    glm::mat4 projection;
+    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
     Renderer renderer;
     /* Loop until the user closes the window */
+    GLCall(glEnable(GL_DEPTH_TEST));
+
+    const float cameraSpeed = 2.5f;
+
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    float deltaTime = 0.0f;	// Time between current frame and last frame
+    float lastFrame = 0.0f; // Time of last frame
+
     while (!glfwWindowShouldClose(window))
     {
-        glm::mat4 trans = glm::mat4(1.0f);
-        trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
-        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-        /* Render here */
-        shader.SetUniformMatrix4fv("u_transform", glm::value_ptr(trans));
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
         renderer.Clear();
-        renderer.Draw(va, ib, shader);
+        /* Render here */
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3((float)glfwGetTime()/2, (float)glfwGetTime() / 2, 0.0f));
+
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        shader.SetUniformMatrix4fv("u_View", glm::value_ptr(view));
+        shader.SetUniformMatrix4fv("u_Projection", glm::value_ptr(projection));
+
+        // Camera stuff (to be abstracted)
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+            cameraPos += cameraSpeed * cameraFront * deltaTime;
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+            cameraPos -= cameraSpeed * cameraFront * deltaTime;
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+            cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed * deltaTime;
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+            cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed * deltaTime;
+
+
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shader.SetUniformMatrix4fv("u_Model", glm::value_ptr(model));
+            renderer.Draw(va, ib, shader);
+        }
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
